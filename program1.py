c2a7b96e34a89109221180ad94ccaec321474a59
shekar-chandra <47810059+shekar-chandra@users.noreply.github.com>
2019-02-20 14:39:22 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,18 @@
+#  A simple program to demonstrate variable in python
+# assigning values to variable
+count=100
+miles=1000.0
+name="chandra"
+ #prinnt the values of the variables.
+print(count)
+print(miles)
+print(name)
+
+
+#mutiple assignments
+a,b=2,3
+print(a)
+print(b)
+
+
+#
